34b279e4a6b2cf506f0b6f5adcc32f9e6649eadf
Nicolas Agudelo <nicoalbert95@hotmail.com>
2022-05-26 12:06:18 -0400

    Added test objects

---
+++ script.py
@@ -0,0 +1,7 @@
+# Author: Nicolas Agudelo
+# Instructions for this project can be found at: https://www.codecademy.com/paths/computer-science/tracks/cspath-cs-101/modules/cspath-boredless-tourist/projects/the-boredless-tourist
+
+destinations = ["Paris, France", "Shangai, China","Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
