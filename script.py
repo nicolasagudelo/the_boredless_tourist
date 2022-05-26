9adf9950e3497f8d7910d241bdbf53e1057fb04b
Nicolas Agudelo <nicoalbert95@hotmail.com>
2022-05-26 12:26:11 -0400

    Added logic to find traveler destinations and convert to internal data

--- script.py
+++ script.py
@@ -1,7 +1,24 @@
 # Author: Nicolas Agudelo
 # Instructions for this project can be found at: https://www.codecademy.com/paths/computer-science/tracks/cspath-cs-101/modules/cspath-boredless-tourist/projects/the-boredless-tourist
 
-destinations = ["Paris, France", "Shangai, China","Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+destinations = ['Paris, France', 'Shanghai, China','Los Angeles, USA', 'Sao Paulo, Brazil', 'Cairo, Egypt']
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
+def get_destination_index(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
+
+def get_traveler_location(traveler):
+    traveler_destination = test_traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+
+# print(get_destination_index('Los Angeles, USA'))
+# print(get_destination_index('Paris, France'))
+# print(get_destination_index('“Hyderabad, India”'))
+
+test_destination_index = get_traveler_location(test_traveler)
+
+print(test_destination_index)
