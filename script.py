58b3073d3b6396dc7ab36c0e2a13875ec4dc6b5b
Nicolas Agudelo <nicoalbert95@hotmail.com>
2022-05-26 15:59:51 -0400

    Added interest finder logic

--- script.py
+++ script.py
@@ -2,12 +2,13 @@
 # Instructions for this project can be found at: https://www.codecademy.com/paths/computer-science/tracks/cspath-cs-101/modules/cspath-boredless-tourist/projects/the-boredless-tourist
 
 # Global Variables
+
 destinations = ['Paris, France', 'Shanghai, China','Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 attractions = [[] for destination in destinations]
-
+attractions_with_interest = []
 
 # Functions
 def get_destination_index(destination):
@@ -24,6 +25,16 @@ def add_attraction(destination, attraction):
     attractions_for_destination = attractions[destination_index]
     attractions_for_destination.append(attraction)
 
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    for attraction in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = attraction[1]
+        for interest in interests:
+            if interest in attraction_tags: attractions_with_interest.append(possible_attraction[0])
+    
+    return attractions_with_interest
 
 # print(get_destination_index('Los Angeles, USA'))
 # print(get_destination_index('Paris, France'))
@@ -45,3 +56,6 @@ add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]]
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 # print(attractions)
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
